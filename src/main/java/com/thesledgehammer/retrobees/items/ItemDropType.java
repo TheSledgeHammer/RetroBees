d17a5c7caef7f60bfeee205632aed2c5c6ecdac8
TheSledgeHammer <martin.kelly4000@gmail.com>
2018-11-30 12:49:20 +1100

    Copyright Header Added

--- src/main/java/com/thesledgehammer/retrobees/items/ItemDropType.java
+++ src/main/java/com/thesledgehammer/retrobees/items/ItemDropType.java
@@ -1,3 +1,11 @@
+/**************************************************************************************************
+ * Copyright (c) 2018 TheSledgeHammer.                                                            *
+ * All rights reserved. This program and the accompanying materials                               *
+ * are made available under the terms of the GNU Lesser Public License v3                         *
+ * which accompanies this distribution, and is available at                                       *
+ * http://www.gnu.org/licenses/lgpl-3.0.txt                                                       *
+ **************************************************************************************************/
+
 package com.thesledgehammer.retrobees.items;
 
 import com.thesledgehammer.retrobees.init.ModItems;

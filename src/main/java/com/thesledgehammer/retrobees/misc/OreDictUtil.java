acf7c644be8243976a990d2809e87202c6a978af
TheSledgeHammer <TheSledgeHammer@users.noreply.github.com>
2018-12-27 22:52:10 +1100

    OreDic Mistake

--- src/main/java/com/thesledgehammer/retrobees/misc/OreDictUtil.java
+++ src/main/java/com/thesledgehammer/retrobees/misc/OreDictUtil.java
@@ -3,5 +3,5 @@ package com.thesledgehammer.retrobees.misc;
 public class OreDictUtil {
 
     public static final String BEE_COMB = "beeComb";
-    public static final String DROP_HONEYDEW = "dropHoneydew";
+    public static final String DROP_HONEY = "dropHoney";
 }
